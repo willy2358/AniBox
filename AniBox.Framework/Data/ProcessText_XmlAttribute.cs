2a43c4e66d56aced83f0a08c22d325a6da156193
willy2358 <willy2358@139.com>
2017-03-08 23:42:19 +0800

    add IProcessText

---
+++ AniBox.Framework/Data/ProcessText_XmlAttribute.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AniBox.Framework.Data
+{
+    class ProcessText_XmlAttribute : IProcessText
+    {
+        public string Name
+        {
+            get { return "Extract Xml Tag Attribute"; }
+        }
+    }
+}
---
+++ AniBox.Framework/Data/ProcessText_XmlInner.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AniBox.Framework.Data
+{
+    class ProcessText_XmlInner : IProcessText
+    {
+        public string Name
+        {
+            get { return "Extract Xml Tag Inner Text"; }
+        }
+    }
+}
